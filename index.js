f4e86f06650168091426dfc78bd30c9e7ba8f4e0
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-06-03 10:42:32 +0600

    get package by id

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ async function run() {
       res.send(result);
     });
 
+    // get individual package by id
+    app.get("/packages/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await pakagesCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
