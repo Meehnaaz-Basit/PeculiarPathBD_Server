711078d7203a62c503e929456ea1efdcc571a84b
Meehnaaz Basit <mhnzbasit.0002@gmail.com>
2024-06-03 10:45:07 +0600

    get tour guide by id

--- index.js
+++ index.js
@@ -55,6 +55,14 @@ async function run() {
       res.send(result);
     });
 
+    // get tour guide by id
+    app.get("/tourGuides/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await tourGuidesCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
